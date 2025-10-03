2c81cd0feed74a579891667a47d99404b0ee2fd7
Casal <javierlolcasal@gmail.com>
2025-10-03 11:05:00 +0200

    feat(boletin6_9.py): Ejercicio acabado

---
+++ Boletin 6/Boletin6_9.py
@@ -0,0 +1,7 @@
+#9 Escribir un programa que almacene os vectores (1,2,3) e (-1,0,2)
+# en duas listas e mostre por pantalla o seu producto escalar.
+
+l1 = [1,2,3]
+l2 = [-1,0,2]
+
+print(l1[0]*l2[0]+l1[1]*l2[1]+l1[2]*l2[2])
