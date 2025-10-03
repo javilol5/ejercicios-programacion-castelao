46163e377686960536fbbc0edc9f3da7e02da06a
Casal <javierlolcasal@gmail.com>
2025-10-03 10:58:25 +0200

    feat(boletin6_8.py): Ejercicio acabado

---
+++ Boletin 6/Boletin6_8.py
@@ -0,0 +1,8 @@
+#  8 Escribir un programa que almacene nunha lista os seguintes prezos,
+#  50, 75, 46, 22, 80, 65, 8,
+#  e mostre por pantalla o menor e o maior dos prezos.
+
+prezos = [50, 75, 46, 22, 80, 65, 8]
+
+prezos.sort()
+print("El prezio mas bajo es: ",prezos[0],"El precio mas alto es: ",prezos[-1])
