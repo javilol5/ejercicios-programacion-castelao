e9b4fae21f9884d5cc55c5ad52b82796ab947edc
casal <javierlolcasal@gmail.com>
2025-10-05 12:02:12 +0200

    feat: :sparkles: Boletin7_7.py
    acabado

---
+++ Boletin 7/Boletin7_7.py
@@ -0,0 +1,8 @@
+#  7 Transforma a cadea de texto “ Phytoneros” a maiúsculas.
+#    Garda o valor na variable e posteriormente convértea novamente a minúsculas.
+
+string = " Phytoneros"
+string_mayus = string.upper()
+print(string_mayus)
+string_minus = string_mayus.lower()
+print(string_minus)
