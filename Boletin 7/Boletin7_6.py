c5477b8cb8a209f8be824dc257baa221253f4e93
casal <javierlolcasal@gmail.com>
2025-10-05 12:00:10 +0200

    feat: :sparkles: Boletin7_6.py
    acabado
    
    dudas de si esta hecho como intencionado

--- Boletin 7/Boletin7_6.py
+++ Boletin 7/Boletin7_6.py
@@ -1,1 +1,11 @@
-#  6 Divide a cadea de texto “ www. phytonparatodos. com” en duas partes “ www. phyton” e “paratodos. com”. Para posteriormente concaténalas e mostralas de novo.
+  # 6 Divide a cadea de texto “ www. phytonparatodos. com” en duas partes “ www. phyton” e “paratodos. com”.
+  #   Para posteriormente concaténalas e mostralas de novo.
+
+string = " www. phytonparatodos. com"
+
+parte1 = string[0:11]
+parte2 = string[11:26]
+
+print(parte1 + parte2)
+
+# Preguntar como hacer
