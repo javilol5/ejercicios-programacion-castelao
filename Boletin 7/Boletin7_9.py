f0f2d872d92b3ec5ab087924506f11d68d1f5640
casal <javierlolcasal@gmail.com>
2025-10-05 12:08:40 +0200

    feat: :sparkles: Boletin7_9.py
    acabado

---
+++ Boletin 7/Boletin7_9.py
@@ -0,0 +1,6 @@
+#  9 Sobre a cadea de texto “ Jeve jeve jeve”, 
+#    substitúe todas as vocais e pola vogal para dando como resultado “java java java”.
+
+string = " Jeve jeve jeve"
+string = string.replace("e", "a")
+print(string)
