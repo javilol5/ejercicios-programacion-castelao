5924fbf8d472b31d8a25802e6fab96fcfbe2115f
Casal <javierlolcasal@gmail.com>
2025-10-10 10:49:01 +0200

    refactor(sinespacio.py): quité el contador de i porque siempre va a ser 0

--- ejercicios clase/sinespacio.py
+++ ejercicios clase/sinespacio.py
@@ -1,11 +1,10 @@
 #quitar espacios antes y despues sin usar stim ni strip
 
 frase = str(input("Ingresa una frase: "))
-i = 0
-while frase[i] == " ":
+while frase[0] == " ":
     frase = frase[1:]
 frase = frase[::-1]
-while frase[i] == " ":
+while frase[0] == " ":
     frase = frase[1:]
 frase = frase[::-1]
 print(frase)
