4c9701645eb15041630f5e7840efab7958e42a73
casal <javierlolcasal@gmail.com>
2025-10-16 23:46:21 +0200

    feat(Boletin8_10.py): :sparkles: Acabado

---
+++ Boletin 8/Boletin8_10.py
@@ -0,0 +1,26 @@
+#10 Escribir unha función que reciba duas matrices e devolte a suma.
+def sumar_matrices(matriz1, matriz2):
+    suma=[]
+    for n1, n2 in zip(matriz1, matriz2):
+        suma_comb = []
+        for i in range(len(n1)):
+            suma_comb.append(n1[i] + n2[i])
+        suma.append(suma_comb)
+    return suma
+        
+    
+
+# Exemplo de uso
+matriz_a = [
+    [1, 2, 3],
+    [4, 5, 6],      
+    [7, 8, 9]
+]
+matriz_b = [
+    [9, 7, 5],
+    [3, 4, 1],
+    [6, 8, 2]
+]
+suma_matrices = sumar_matrices(matriz_a, matriz_b)
+print(suma_matrices)
+# Saída: [[10, 9, 8], [7, 9, 7], [13, 16, 11]]
