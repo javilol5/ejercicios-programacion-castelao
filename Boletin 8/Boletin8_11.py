c94bd37c94c39f8d457307083a0043c03bbcc3d1
Casal <javierlolcasal@gmail.com>
2025-10-17 09:06:22 +0200

    refactor(boletin8_11.py): :sparkles: Ejercicio Acabado Antes estaba el 8_10.2 , ahora solo está el 8_11

--- Boletin 8/Boletin8_11.py
+++ Boletin 8/Boletin8_11.py
@@ -1,30 +1,26 @@
-#11 Escribir unha función que reciba duas matrices e devolte o produto.
-#Pregado de un texto. 
+#11 Pregado de un texto.
 # Escribir unha función que reciba un texto e unha lonxitude e devolva unha lista de cadeas de como máximo esa lonxitude. 
 # As líñas deben cortarse correctamente nos espazos (sen cortar as palabras).
 
-def mult_matrices(matriz1, matriz2):
+def pregado_texto(texto, lonxitude):
 
-    mult=[]
-    for n1, n2 in zip(matriz1, matriz2):
-        mult_comb = []
-        for i in range(len(n1)):
-            mult_comb.append(n1[i] * n2[i])
-        mult.append(mult_comb)
-    return mult
-        
-    
+    texto = texto.split()
+    counter = 0
+    resultado = []
+    linea_actual = ""
+    for palabra in texto:
+        if (len(palabra) + counter) <= lonxitude:
+            linea_actual += palabra + " "
+            counter += len(palabra) + 1
+        else:
+            resultado.append(linea_actual.strip())
+            linea_actual = palabra + " "
+            counter = len(palabra) + 1
+    resultado.append(linea_actual.strip())
+    return resultado
 
 # Exemplo de uso
-matriz_a = [
-    [1, 2, 3],
-    [4, 5, 6],      
-    [7, 8, 9]
-]
-matriz_b = [
-    [9, 7, 5],
-    [3, 4, 1],
-    [6, 8, 2]
-]
-resultado = mult_matrices(matriz_a, matriz_b)
+texto = "Este é un exemplo de texto que será pregado en liñas de lonxitude específica."
+lonxitude = 20
+resultado = pregado_texto(texto, lonxitude)
 print(resultado)
