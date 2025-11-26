c6a74769097bc991c847b729c1b2dc2f41bd17f9
Casal <javierlolcasal@gmail.com>
2025-11-26 14:30:09 +0100

    feat(horas POO): creacion del main y comienzo de tiempo.py

---
+++ ejercicios clase/POO/horas/main.py
@@ -0,0 +1,14 @@
+from tiempo import Data
+
+h1 = Data(29,2,2017)
+h2 = Data(30,4,2021)
+h3 = Data(29,2,2024)
+h4 = Data(1,1,2000)
+h5 = Data(30,2,2019)
+
+
+print(h1)
+print(h2)
+print(h3)
+print(h4)
+print(h5)
---
+++ ejercicios clase/POO/horas/tiempo.py
@@ -0,0 +1,5 @@
+class Tiempo(Data):
+    def __init__(self, s, m, h):
+        self.s = s
+        self.m = m
+        self.h = h
